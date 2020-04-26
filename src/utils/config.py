5e5a7e80ad7ee502f4a7ce5e77d6bdd3934ad7bd
zhenqzhao <zhenqzhao@ebay.com>
2020-04-26 15:33:23 +0800

    add config and code structure

---
+++ src/utils/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time   : 2020-04-26 14:54
+@Author : Kyrie.Z
+@File   : config.py
+"""
+
+import os
+
+
+class Configuration(object):
+    def __init__(self, base_path, suffix):
+        self.data_prefix = os.path.join(base_path, 'data', suffix)
+        self.cache_prefix = os.path.join(base_path, 'cache', suffix)
+
+        self.edge_file = os.path.join(self.data_prefix, 'edges.txt')
+        self.edge_cache = os.path.join(self.cache_prefix, 'edges.pkl')
+        self.edge_label_file = os.path.join(self.data_prefix, 'edge_labels.txt')
+        self.edge_label_cache = os.path.join(self.cache_prefix, 'edge_labels.pkl')
+
+
+if __name__ == '__main__':
+    config = Configuration('../../', suffix='ebay-mlc')
+    print(config.edge_file)
+    assert config.edge_file == '../../data/ebay-mlc/edges.txt'
