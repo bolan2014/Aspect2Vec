1bf92e786f62196205c7c479f5248a94efd6ea7b
yuchguo1007 <mf1814036@smail.nju.edu.cn>
2020-04-27 14:48:21 +0800

    cleaned mlc data

--- src/utils/config.py
+++ src/utils/config.py
@@ -18,6 +18,10 @@ class Configuration(object):
         self.edge_cache = os.path.join(self.cache_prefix, 'edges.pkl')
         self.edge_label_file = os.path.join(self.data_prefix, 'edge_labels.txt')
         self.edge_label_cache = os.path.join(self.cache_prefix, 'edge_labels.pkl')
+        self.mlc_origin_data = os.path.join(self.data_prefix, 'validation_set.tsv')
+        self.flipkart_origin_data = os.path.join(self.data_prefix, 'flipkart_com-ecommerce_sample.csv')
+        self.mlc_data_file = os.path.join(self.data_prefix, 'mlc_dataset.tsv')
+        self.flipkart_data_file = os.path.join(self.data_prefix, 'fk_dataset.csv')
 
 
 if __name__ == '__main__':
