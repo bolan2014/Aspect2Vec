6b32bd8ec71ba72c2924dbda22ea7f42682a9edd
zhenqzhao <zhenqzhao@ebay.com>
2020-04-28 22:15:56 +0800

    implement method of extracting edges from item aspect

--- src/extra_graph_from_aspect.py
+++ src/extra_graph_from_aspect.py
@@ -6,6 +6,7 @@
 @File   : extra_graph_from_aspect.py
 """
 
+import pickle
 from tqdm import tqdm
 from collections import Counter
 from sklearn import preprocessing
@@ -13,14 +14,16 @@ from sklearn import preprocessing
 from src.utils.config import Configuration
 
 
-class GraphBuilder:
-    def __init__(self, cf: Configuration, delimiter: str='|'):
+class GraphExtractor:
+    def __init__(self, cf: Configuration, delimiter: str = '|', dump_file: bool = False):
         self.cf = cf
+        self.logging = cf.logging
         self.delimiter = delimiter
+        self.dump_file = dump_file
         self.item_aspect = self.get_aspect_from_file()
         self.aspects = self.flatten()
-        self.encoder = self.build_encoder()
-        # self.counter = Counter()
+        self.aspect_id_mapping, self.id_aspect_mapping = self.build_encoder()
+        self.edges = self.make_edges()
 
     def get_aspect_from_file(self):
         import pandas as pd
@@ -28,38 +31,98 @@ class GraphBuilder:
 
         # TODO: Do some filtering and constraint about the dataframe, and get their aspects
         aspect_series = df['aspects']
-        self.cf.logging.info('get item aspects from file')
+        self.logging.info('get item aspects from file: {}'.format(self.cf.cleaned_data_file))
         item_aspect = []
         for aspect_line in tqdm(aspect_series):
             # TODO: consider whether need (val -> key) map or not
             item_aspect.append(
                 aspect_line.split(self.delimiter)
             )
-        self.cf.logging.info('item size: {}'.format(len(item_aspect)))
+        self.logging.info('item size: {}'.format(len(item_aspect)))
         return item_aspect
 
     def flatten(self):
         total_aspects = [aspect for aspects in self.item_aspect for aspect in aspects]
         # example: ['inseam: 28', 'size type: regular', 'rise: mid-rise', 'wash: colored']
-        self.cf.logging.info('total aspect size: {}'.format(len(total_aspects)))
+        self.logging.info('total aspect size: {}'.format(len(total_aspects)))
         return total_aspects
 
     def build_encoder(self):
+        """
+        :return: two mappings
+        """
         encoder = preprocessing.LabelEncoder()
-        self.cf.logging.info('build node encoder ...')
+        self.logging.info('build node encoder ...')
         encoder.fit(self.aspects)
-        return encoder
+
+        classes, ids = encoder.classes_, encoder.transform(encoder.classes_)
+        id_aspect_mapping = dict(zip(ids, classes))
+        aspect_id_mapping = dict(zip(classes, ids))
+        try:
+            assert len(id_aspect_mapping) == len(aspect_id_mapping)
+        except AssertionError:
+            self.logging.debug('mapping size not equal: {} vs {}'.format(len(id_aspect_mapping), len(aspect_id_mapping)))
+        self.logging.info('unique aspect size: {}'.format(len(id_aspect_mapping)))
+        return aspect_id_mapping, id_aspect_mapping
+
+    def make_pairs(self, aspects: list):
+        """
+        make aspect pairs with order
+        :param aspects:
+        :return: aspect pairs from the same item
+        """
+        pairs = []
+        for i in range(len(aspects)):
+            for j in range(i+1, len(aspects)):
+                a_i, a_j = self.aspect_id_mapping[aspects[i]], self.aspect_id_mapping[aspects[j]]
+                if aspects[i] < aspects[j]:
+                    pairs.append((a_i, a_j))
+                else:
+                    pairs.append((a_j, a_i))
+        return pairs
 
     def make_edges(self):
-        # the frequency is denoted as edge weight
-        pass
+        """
+        the frequency is denoted as edge weight
+        :return: tuple of numbers
+        """
+        edges = []
+        for aspects in tqdm(self.item_aspect):
+            if len(aspects) > 1:
+                edges.extend(self.make_pairs(aspects=aspects))
+        return edges
+
+    def dump_to_file(self):
+        self.logging.info('total edges size: {}'.format(len(self.edges)))
+        edges_with_weight = dict(Counter(self.edges))
+        self.logging.info('unique edges size: {}'.format(len(edges_with_weight)))
+
+        if self.dump_file is True:
+            self.logging.info('dump to file ...')
+
+            # dump total edges
+            with open(self.cf.edge_file, 'w') as handler:
+                for edge in self.edges:
+                    handler.write('{} {}\n'.format(edge[0], edge[1]))
+
+            # dump unique edges with weight
+            with open(self.cf.weighted_edge_file, 'w') as handler:
+                for edge, weight in edges_with_weight.items():
+                    handler.write('{} {} {}\n'.format(edge[0], edge[1], weight))
+
+            # dump aspect to id mapping
+            with open(self.cf.aspect_to_id_cache, 'wb') as handler:
+                pickle.dump(self.aspect_id_mapping, handler, protocol=pickle.HIGHEST_PROTOCOL)
+
+            # dump id to aspect mapping
+            with open(self.cf.id_to_aspect_cache, 'wb') as handler:
+                pickle.dump(self.id_aspect_mapping, handler, protocol=pickle.HIGHEST_PROTOCOL)
+
+            self.logging.info('Done')
 
 
 if __name__ == '__main__':
     config = Configuration('../', suffix='ebay-mlc', file_name='validation_set.tsv')
 
-    kg_builder = GraphBuilder(cf=config)
-
-    # cities = ["paris", "paris", "tokyo", "amsterdam"]
-    # node_encoder = NodeEncoder(cities)
-    # print(node_encoder.encoder.transform(cities))
+    kg_extractor = GraphExtractor(cf=config, dump_file=True)
+    kg_extractor.dump_to_file()
--- src/utils/config.py
+++ src/utils/config.py
@@ -20,6 +20,9 @@ class Configuration(object):
         self.cache_prefix = os.path.join(base_path, 'cache', suffix)
 
         self.edge_file = os.path.join(self.data_prefix, 'edges.txt')
+        self.weighted_edge_file = os.path.join(self.data_prefix, 'weighted_edges.txt')
+        self.id_to_aspect_cache = os.path.join(self.cache_prefix, 'id_to_aspect.pkl')
+        self.aspect_to_id_cache = os.path.join(self.cache_prefix, 'aspect_to_id.pkl')
         self.edge_cache = os.path.join(self.cache_prefix, 'edges.pkl')
         self.edge_label_file = os.path.join(self.data_prefix, 'edge_labels.txt')
         self.edge_label_cache = os.path.join(self.cache_prefix, 'edge_labels.pkl')
